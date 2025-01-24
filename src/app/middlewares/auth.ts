728dd4cdada3b4b51efc3fdeb1b719e82e39f4dc
Tawsif05 <dilafro346@gmail.com>
2025-01-24 20:18:18 +0600

    update auth

--- src/app/middlewares/auth.ts
+++ src/app/middlewares/auth.ts
@@ -8,29 +8,29 @@ import jwt, { JwtPayload } from 'jsonwebtoken';
 import User from '../modules/User/user.model';
 
 const auth = (...requiredRoles: TUserRole[]) => {
-  // console.log(requiredRoles, 'requiredRoles');
+  
   return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
-    const getTokenWithBearer = req.headers?.authorization;
-    if (!getTokenWithBearer) {
+    const getToken = req.headers?.authorization;
+    if (!getToken) {
       throw new AppError(
         StatusCodes.UNAUTHORIZED,
         `You are not authorized to access this route `
       );
     }
-    const token = getTokenWithBearer.split(' ')[1] || getTokenWithBearer;
+    const token = getToken.split(' ')[1] || getToken;
 
-    // check validation for token and decode the token
+    
     const decoded = jwt.verify(
       token,
       config.jwt_access_secret as string
     ) as JwtPayload;
-    //    user check from database
+    
     const { email, role } = decoded;
     const user = await User.findOne({ email });
     if (!user) {
       throw new AppError(StatusCodes.NOT_FOUND, 'User not found');
     }
-    // check if the user is blocked
+    
     if (user.isBlocked) {
       throw new AppError(StatusCodes.FORBIDDEN, 'This user is blocked');
     }
@@ -41,7 +41,7 @@ const auth = (...requiredRoles: TUserRole[]) => {
       );
     }
     req.user = decoded;
-    // console.log(req.user,user,decoded, 'req.user');
+    
     next();
   });
 };
