2474fa03a6f327a7f084e671310c80f64d61e768
Tawsif05 <dilafro346@gmail.com>
2025-01-24 11:48:59 +0600

    modify app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,20 @@
-import express, { Application, Request, Response } from 'express';
-import cors from 'cors';
-import router from './app/routes';
-import { globalErrorHandler } from './app/middlewares/globalErrorHandler';
-import notFound from './app/middlewares/notFound';
+import express, { Application, Request, Response } from "express";
+import cors from "cors";
+import router from "./app/routes";
+import { globalErrorHandler } from "./app/middlewares/globalErrorHandler";
+import notFound from "./app/middlewares/notFound";
 const app: Application = express();
 
 app.use(express.json());
 app.use(cors());
-app.use('/api', router);
+app.use("/api", router);
 
 app.use(globalErrorHandler);
-app.use(notFound);
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!');
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World!");
 });
+
+app.use(notFound);
 
 export default app;
